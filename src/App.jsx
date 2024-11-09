c8dccc11670c0b833e3f88bc140872dd57cd7c95
AlishanMasood <alishanmasood20@gmail.com>
2024-11-09 22:04:09 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import StyledHome from "./components/StyledHome";
+import Login from "./pages/auth/Login";
+
+function App() {
+  return (
+    <>
+      {/* <StyledHome /> */}
+      <Login />
+    </>
+  );
+}
+
+export default App;
