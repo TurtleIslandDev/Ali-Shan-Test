af81c9bbd0073e98a0145db25574d31906b73c49
AlishanMasood <alishanmasood20@gmail.com>
2024-11-10 15:26:49 +0500

    routing is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StyledHome from "./components/StyledHome";
 import Login from "./pages/auth/Login";
+import DemoAgentCoachingReport from "./pages/reports/DemoAgentCoachingReport";
 
 function App() {
   return (
     <>
       {/* <StyledHome /> */}
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<StyledHome />} />
+          <Route
+            path="/demo-agent-coaching-report"
+            element={<DemoAgentCoachingReport />}
+          />
+          <Route path="/*" element={<div>Page not found</div>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
