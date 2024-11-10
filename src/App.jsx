2dcb81c91ef4d0eeabc1993ce285aed09c118955
AlishanMasood <alishanmasood20@gmail.com>
2024-11-10 16:51:43 +0500

    new home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import StyledHome from "./components/StyledHome";
 import Login from "./pages/auth/Login";
 import DemoAgentCoachingReport from "./pages/reports/DemoAgentCoachingReport";
+import Homepage from "./pages/home/Homepage";
 
 function App() {
   return (
     <>
-      {/* <StyledHome /> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/home" element={<StyledHome />} />
+          <Route path="/home" element={<Homepage />} />
           <Route
             path="/demo-agent-coaching-report"
             element={<DemoAgentCoachingReport />}
