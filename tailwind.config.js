eddcfea5428c61eb670187ff96fadb3ac9afb89f
AlishanMasood <alishanmasood20@gmail.com>
2024-11-10 15:26:36 +0500

    new layout theme is added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
       backgroundImage: {
         login: "url('/assets/bgImages/bgLogin.png')",
       },
+      maxWidth: {
+        layout: "1226px",
+      },
+      maxWidth: {
+        layout: "1226px",
+      },
+      colors: {
+        whitePara: "#E3E3E3",
+        darkPara: "#3F3F3F",
+      },
     },
   },
   plugins: [],
