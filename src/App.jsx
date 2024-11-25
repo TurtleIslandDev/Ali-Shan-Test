8dd5bc1c7d1c68ddad9769a60a2777c43e9542ce
AlishanMasood <alishanmasood20@gmail.com>
2024-11-25 15:06:10 +0500

    created page for trainig interface

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Homepage from "./pages/home/Homepage";
 import DemoAgentCoachingReportAgentVersion from "./pages/reports/AgentScreens/DemoAgentCoachingReportAgentVersion";
+import AgentSystemsTraining from "./pages/agent-systems-training/AgentSystemsTraining";
 
 function App() {
   return (
@@ -13,6 +14,10 @@ function App() {
           <Route
             path="/demo-agent-coaching-report"
             element={<DemoAgentCoachingReportAgentVersion />}
+          />
+          <Route
+            path="/agent-system-interface"
+            element={<AgentSystemsTraining />}
           />
           <Route path="/*" element={<div>Page not found</div>} />
         </Routes>
