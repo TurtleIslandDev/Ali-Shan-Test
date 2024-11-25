fddbcb491f5a27fc664dee749e628edf3fa967b7
AlishanMasood <alishanmasood20@gmail.com>
2024-11-25 22:42:20 +0500

    added on click

--- src/components/Buttons/IconButton.jsx
+++ src/components/Buttons/IconButton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const IconButton = ({ children, bgColor }) => {
+const IconButton = ({ children, bgColor, onClick }) => {
   console.log(bgColor);
 
   return (
@@ -9,6 +9,7 @@ const IconButton = ({ children, bgColor }) => {
       className={`block font-poppins text-lg font-semibold leading-7 text-center decoration-solid underline-offset-4  bg-[${
         bgColor ? bgColor : ""
       }] rounded-[4px] text-white w-max`}
+      onClick={onClick}
     >
       {children}
     </button>
