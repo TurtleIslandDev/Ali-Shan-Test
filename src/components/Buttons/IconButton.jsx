c0abd725553cfc733eab4cec8d979676ec7bc70e
AlishanMasood <alishanmasood20@gmail.com>
2024-11-25 17:29:58 +0500

    button components are created

--- src/components/Buttons/IconButton.jsx
+++ src/components/Buttons/IconButton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Button = ({ children, bgColor }) => {
+const IconButton = ({ children, bgColor }) => {
   console.log(bgColor);
 
   return (
@@ -8,11 +8,11 @@ const Button = ({ children, bgColor }) => {
       type="button"
       className={`block font-poppins text-lg font-semibold leading-7 text-center decoration-solid underline-offset-4  bg-[${
         bgColor ? bgColor : ""
-      }] px-10 py-[10px] rounded-[4px] text-white w-max`}
+      }] rounded-[4px] text-white w-max`}
     >
       {children}
     </button>
   );
 };
 
-export default Button;
+export default IconButton;
