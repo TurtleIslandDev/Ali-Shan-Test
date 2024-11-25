cbaf5aad45c82a56c2358580350d81d626f830f7
AlishanMasood <alishanmasood20@gmail.com>
2024-11-25 22:49:17 +0500

    background color issue resolved

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -6,7 +6,8 @@ const Button = ({ children, bgColor }) => {
   return (
     <button
       type="button"
-      className={`block font-poppins text-lg font-semibold leading-7 text-center decoration-solid underline-offset-4  bg-[${bgColor}] px-10 py-[10px] rounded-[4px] text-white`}
+      style={{ backgroundColor: bgColor }}
+      className={`block font-poppins text-lg font-semibold leading-7 text-center decoration-solid underline-offset-4   px-10 py-[10px] rounded-[4px] text-white`}
     >
       {children}
     </button>
