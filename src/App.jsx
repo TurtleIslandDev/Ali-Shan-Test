273ce45337c515d03a3d8e810df76c13f117f086
AlishanMasood <alishanmasood20@gmail.com>
2024-11-26 11:39:02 +0500

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./pages/auth/Login";
-import Homepage from "./pages/home/Homepage";
-import DemoAgentCoachingReportAgentVersion from "./pages/reports/AgentScreens/DemoAgentCoachingReportAgentVersion";
-import AgentSystemsTraining from "./pages/agent-systems-training/AgentSystemsTraining";
-
+import { lazy, Suspense } from "react";
+const AgentSystemsTraining = lazy(() =>
+  import("./pages/agent-systems-training/AgentSystemsTraining")
+);
+const DemoAgentCoachingReportAgentVersion = lazy(() =>
+  import("./pages/reports/AgentScreens/DemoAgentCoachingReportAgentVersion")
+);
+const Homepage = lazy(() => import("./pages/home/Homepage"));
+const Login = lazy(() => import("./pages/auth/Login"));
 function App() {
   return (
     <>
+      {/* there is new react-router-version comming so if any issues faced for routing look at the new documentations */}
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Homepage />} />
-          <Route
-            path="/demo-agent-coaching-report"
-            element={<DemoAgentCoachingReportAgentVersion />}
-          />
-          <Route
-            path="/agent-system-interface"
-            element={<AgentSystemsTraining />}
-          />
-          <Route path="/*" element={<div>Page not found</div>} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Homepage />} />
+            <Route
+              path="/demo-agent-coaching-report"
+              element={<DemoAgentCoachingReportAgentVersion />}
+            />
+            <Route
+              path="/agent-system-interface"
+              element={<AgentSystemsTraining />}
+            />
+            <Route path="/*" element={<div>Page not found</div>} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </>
   );
