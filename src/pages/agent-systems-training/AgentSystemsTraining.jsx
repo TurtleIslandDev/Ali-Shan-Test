669d6b10c61339180bd306cb9dedb7314aac777a
AlishanMasood <alishanmasood20@gmail.com>
2024-11-29 13:04:32 +0500

    firstly the height of page is now browser dependent and will adjust according to browser height

--- src/pages/agent-systems-training/AgentSystemsTraining.jsx
+++ src/pages/agent-systems-training/AgentSystemsTraining.jsx
@@ -14,12 +14,16 @@ const AgentSystemsInterface = () => {
   const [micOn, setMicOn] = useState(true);
   const [inBound, setInBound] = useState(true);
   const [selectedDisposition, setSelectedDisposition] = useState("");
+  //this sound result is the value which will automatically increase and decrease the value of green in the bottom voice bar
+  //max value is 40 and 0 is optimal
+  let soundResult = 0;
   const handleMic = () => {
     setMicOn(!micOn);
   };
   const handleCall = () => {
     setInBound(!inBound);
   };
+
   let dispositionData = [
     {
       id: "1",
@@ -46,7 +50,7 @@ const AgentSystemsInterface = () => {
   ];
   return (
     <div
-      className="w-full px-20"
+      className="w-full px-20 h-screen"
       style={{
         backgroundImage: `url(${bg})`,
         backgroundRepeat: "no-repeat",
@@ -54,9 +58,9 @@ const AgentSystemsInterface = () => {
         backgroundPosition: "center",
       }}
     >
-      <div className="bg-[#121729]">
-        <div className="flex gap-5">
-          <div className="w-full max-w-[425px] bg-white flex flex-col items-center justify-between p-4 gap-16">
+      <div className="bg-[#121729] h-screen">
+        <div className="flex gap-5 h-[86vh]">
+          <div className=" w-full max-w-[425px] bg-white flex flex-col items-center justify-between px-4 pb-4 ">
             <div className="flex shadow-md rounded-xl w-full ">
               <div>
                 <DonutChart />
@@ -69,7 +73,7 @@ const AgentSystemsInterface = () => {
             <IconButton onClick={handleMic}>
               {micOn ? <MicSvg /> : <MuteMicSvg />}
             </IconButton>
-            <div className="flex flex-col gap-4 mx-4 mt-4">
+            <div className="flex flex-col mx-4 gap-2 ">
               <Button bgColor={"#F5760E"}>Customer Care</Button>
               <Button bgColor={"#1E40AF"}>Prospect</Button>
               <Button bgColor={"#228512"}>Sales</Button>
@@ -126,27 +130,34 @@ const AgentSystemsInterface = () => {
             <div className=" bg-white w-full flex h-[calc(100%-100px)]"></div>
           </div>
         </div>
-        <div>
-          <div className="flex justify-end items-center h-[145px] pr-6 gap-20">
-            <div
-              className="relative w-[707px] h-4  rounded-full"
-              style={{
-                background: `linear-gradient(270deg, #FA6F05 0.78%, #FEC300 31.18%, #7EC73E 38.63%, #70C744 50%, #75C442 61.78%, #FEBD00 69.92%, #FF6C03 100%)`,
-              }}
-            >
-              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-9 h-9 bg-white rounded-full border-4 border-gray-300 cursor-pointer flex items-center justify-center">
-                <div className="bg-green-500 w-3 h-3 rounded-full"></div>
-              </div>
-              <div className="absolute top-1/2 left-1/3 transform -translate-x-1/2 -translate-y-1/2 w-1 h-10 bg-white rounded-full border-4 border-gray-300 cursor-pointer flex items-center justify-center"></div>
-              <div className="absolute top-1/2 left-2/3 transform -translate-x-1/2 -translate-y-1/2 w-1 h-10 bg-white rounded-full border-4 border-gray-300 cursor-pointer flex items-center justify-center"></div>
+
+        <div className="flex justify-end items-center h-[14vh] gap-4">
+          <div
+            className="relative w-[707px] h-4  rounded-full"
+            style={{
+              background: `linear-gradient(
+                270deg,
+                rgba(250, 111, 5, 1) 0%,
+                rgba(254, 195, 0, 1) ${30.63 - soundResult}%,
+                rgba(126, 199, 62, 1) ${38.14 - soundResult}%,   
+                rgba(117, 196, 66, 1) ${61.47 + soundResult}%,
+                rgba(254, 189, 0, 1) ${69.68 + soundResult}%,
+                rgba(255, 108, 3, 1) 100%
+              )`,
+            }}
+          >
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-9 h-9 bg-white rounded-full border-4 border-gray-300 cursor-pointer flex items-center justify-center">
+              <div className="bg-green-500 w-3 h-3 rounded-full"></div>
             </div>
-            {/* <Dropdown /> */}
-            <CustomDropdown
-              selectedDisposition={selectedDisposition}
-              setSelectedDisposition={setSelectedDisposition}
-              dispositionData={dispositionData}
-            />
+            <div className="absolute top-1/2 left-1/3 transform -translate-x-1/2 -translate-y-1/2 w-1 h-10 bg-white rounded-full border-4 border-gray-300 cursor-pointer flex items-center justify-center"></div>
+            <div className="absolute top-1/2 left-2/3 transform -translate-x-1/2 -translate-y-1/2 w-1 h-10 bg-white rounded-full border-4 border-gray-300 cursor-pointer flex items-center justify-center"></div>
           </div>
+          {/* <Dropdown /> */}
+          <CustomDropdown
+            selectedDisposition={selectedDisposition}
+            setSelectedDisposition={setSelectedDisposition}
+            dispositionData={dispositionData}
+          />
         </div>
       </div>
     </div>
