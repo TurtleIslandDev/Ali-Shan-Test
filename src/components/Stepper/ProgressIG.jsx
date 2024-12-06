c2d84fe8596037cfda264c3361814a5f8da6e664
AlishanMasood <alishanmasood20@gmail.com>
2024-12-06 16:42:04 +0500

    new component created for progress

---
+++ src/components/Stepper/ProgressIG.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function ProgressIG() {
+  return (
+    <div className="flex w-full flex-col gap-4 bg-customGradient h-3 rounded-xl">
+      <div className={`w-[17%] h-full bg-custom-green-gradient rounded-xl`}>
+        {" "}
+      </div>
+    </div>
+  );
+}
