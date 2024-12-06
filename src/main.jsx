7e55deff30f6a663e219b4f4dbe017b418e1d596
AlishanMasood <alishanmasood20@gmail.com>
2024-12-06 16:44:55 +0500

    theme provider is wraped on app

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,7 @@ import { ThemeProvider } from "@material-tailwind/react";
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <ThemeProvider>
-
-  <App />
+    <App />
   </ThemeProvider>
-  // </StrictMode>
+  /* </StrictMode> */
 );
