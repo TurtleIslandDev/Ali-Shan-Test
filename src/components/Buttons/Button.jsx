db98a19f0a3133160b0f4fe7c514883036a1facc
AlishanMasood <alishanmasood20@gmail.com>
2024-12-24 02:29:23 +0500

    used react memo for rerendring issues

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 
-const Button = ({ children, bgColor,onClick,type }) => {
-  console.log(bgColor);
-
+const Button = React.memo(({ children, bgColor, onClick, type, className }) => {
   return (
     <button
-      type={type?type:"button"}
+      type={type ? type : "button"}
       style={{ backgroundColor: bgColor }}
-      className={`font-poppins text-lg font-semibold leading-7 text-center decoration-solid underline-offset-4   px-5 py-[10px] rounded-[4px] text-white flex gap-2.5 items-center`}
-onClick={onClick}
+      className={`font-poppins text-lg font-semibold leading-7 text-center decoration-solid underline-offset-4 px-5 py-[10px] rounded-[4px] text-white flex gap-2.5 items-center ${className}`}
+      onClick={onClick}
     >
       {children}
     </button>
   );
-};
+});
 
 export default Button;
