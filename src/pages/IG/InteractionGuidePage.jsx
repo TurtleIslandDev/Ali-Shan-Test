efb05a3beb9f059d8003c7d753788daee9ea055f
AlishanMasood <alishanmasood20@gmail.com>
2025-02-20 05:52:32 +0500

    window hook is removed and added to hooks folder

--- src/pages/IG/InteractionGuidePage.jsx
+++ src/pages/IG/InteractionGuidePage.jsx
@@ -1,22 +1,11 @@
 import React, { useEffect, useState } from "react";
 import InteractionGuide from "./../../components/IG/InteractionGuide";
+import useWindowHeight from "../../features/hooks/useWindowHeight";
 
-const useWindowHeight = () => {
-  const [height, setHeight] = useState(window.innerHeight);
-
-  useEffect(() => {
-    const handleResize = () => setHeight(window.innerHeight);
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  return height;
-};
 const InteractionGuidePage = () => {
   const height = useWindowHeight();
   return (
-    <div style={{ background: "blue", height: `${height}px` }}>
+    <div style={{ height: `${height}px` }}>
       <InteractionGuide />
     </div>
   );
